93d0a14e9e37cf640e5e3e588be19d069f4af89e
taiwobenedict <ajayitaiwotunta@gmail.com>
2023-08-07 18:07:06 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// Smooth scrolling
+$("nav a").on("click", function (e) {
+    e.preventDefault();
+    const href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(href).offset().top -50}, 500);
+  });
+  
+
+  
+  const header = document.querySelector('header')
+  let toggler = document.querySelector('.toggler');
+  
+  toggler.onclick = ()=> {
+    toggler.firstChild.classList.toggle("fa-times")
+    toggler.firstChild.classList.toggle("fa-bars")
+  }
+  
